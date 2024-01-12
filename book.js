45f028e0a576e2aa6f79748e29341bb0b223f21d
Dario Russo <dariorusso987@gmail.com>
2024-01-12 17:58:36 +0100

    up

--- book.js
+++ book.js
@@ -8,7 +8,7 @@ export const getBooks = (item) => {
  return `
 	<div class="col-6 col-md-4 col-lg-3">
      <div class="card mt-4 ">
-        <div class="ratio ratio-16x9"><img src=${item.img} class="card-img-top mt-2" alt="image" /></div>
+        <div class="ratio ratio-16x9"><img src=${item.img} class="card-img-top" alt="image" /></div>
         <div class="card-body">
          <h5 class="card-title text-truncate">${item.title}</h5>
         </div>
